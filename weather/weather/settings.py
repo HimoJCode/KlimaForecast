66faf7a4dc9c0e46e4644c8d6fc1deead20d3049
Himo <jerameel.himo2001@gmail.com>
2024-03-31 22:52:06 +0800

    hosting in vercel

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x85h18iqanpbd9k6!s8u-vard%pi844_5oc0^iw(x&@@l!dqah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'weather.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GhUSwShgPkiLvpliuzAVoBNHaEIGMcLx',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '17312',
     }
 }
 
@@ -106,7 +110,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
